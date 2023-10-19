07590b5afca3fd257f37587dace650f614339892
Maya Saltzman <mayasaltzman3@gmail.com>
2023-10-19 09:24:47 -0400

    created sql tbales

---
+++ habitsql.py
@@ -0,0 +1,45 @@
+import os
+import sqlite3
+
+class Database:
+    
+    #creates a data base connection and stores it as a class attribute
+    #if reset is set to true it deletes old database and creates a fresh one
+    def __init__(self, reset = False):
+        
+        #if reset is true and database exists remove it
+        if(reset == True):
+            if os.path.exists('habits.db'):
+                os.remove('habits.db')
+
+        self.conn = sqlite3.connect('habits.db') #creates and opens new database connection
+
+    #create tables
+        #habit table
+            #cols: id, habit, category, frequency (daily, weekly, monthly), streak
+        #category table
+            #cols: category id, category name
+    def create_tables(self):
+        
+        #Habit table
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Habits 
+                 ( ID     INTEGER NOT NULL,
+                   HABIT  VARCHAR NOT NULL,
+                   CATEGORY  VARCHAR,
+                   FREQUENCY  VARCHAR NOT NULL,
+                   STREAK    INTEGER NOT NULL,
+                   PRIMARY KEY (ID) );""" )
+        
+        #Category table
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Categories 
+                 ( ID     INTEGER NOT NULL,
+                   CATEGORY_NAME VARCHAR,
+                   PRIMARY KEY (ID) );""" )
+    
+    #set items
+    
+    #add habits
+    
+    #get habits
+    
+    # def main():
