90a3524d9a1c9f1e7c513f8485831a0360b518ee
Maya Saltzman <mayasaltzman3@gmail.com>
2023-10-24 15:20:30 -0400

    now a react app

--- habit.py
+++ habit.py
@@ -2,13 +2,14 @@
 class Habits:
     #pair category with habit
 
-    #complete habit
+    #complete habit and count streak
     def complete_habit(self,status):
         if status == 0:
             return True
         else:
             return False
 
-    #count streak
+    
+        
 
 
