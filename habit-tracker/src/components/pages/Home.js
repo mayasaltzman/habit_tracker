16b696dd68f6f66fe4b7ef3b79f38f485d816db4
Maya Saltzman <mayasaltzman3@gmail.com>
2023-10-26 17:11:39 -0400

    fixed date and time problem with moment import

--- habit-tracker/src/components/pages/Home.js
+++ habit-tracker/src/components/pages/Home.js
@@ -1,30 +1,36 @@
-// need to fix MINUTES ONLY UPDATING ON REFRESH
-
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 
 const Home = () => {
-    const [date, setDate] = useState(new Date()) //used to set current date and time
+    // const [date, setDate] = useState(new Date()) //used to set current date and time
 
-    useEffect(() => {
-        const timer = setInterval(() => { setDate(new Date()) }, 60000) //date is at an interval of 60 000 ms so clock updates each minute
+    // useEffect(() => {
+    //     const timer = setInterval(() => { setDate(new Date()) }, 60000) //date is at an interval of 60 000 ms so clock updates each minute
 
-        return () => clearInterval(timer)
-        // return function cleanup() { //helps avoid memeory leaks
-        //     clearInterval(timer)
-        // }
+    //     // return () => clearInterval(timer)
+    //     return function cleanup() { //helps avoid memeory leaks
+    //         clearInterval(timer)
+    //     }
 
-    })
+    // })
 
     // getting date and time values
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
+    // const hours = date.getHours()
+    // const minutes = date.getMinutes()
+    
+    //moment library just does this for me lol
+    const curr = moment().format('MMMM Do YYYY')
+    const time = moment().format('h:mm a')
 
     return (
         <div className='welcome'>
             {/* extracts time and returns as string */}
-            <p>{String(hours - 12).padStart(2, '0')}:{String(minutes).padStart(2, '0')}</p>
+            {/* <p>{String(hours - 12).padStart(2, '0')}:{String(minutes).padStart(2, '0')}</p> */}
             {/* extracts datw and returns as string */}
-            <p>{date.toLocaleDateString()}</p>
+            {/* <p>{date.toLocaleDateString()}</p> */}
+            <h1>Welcome</h1>
+            <p>{curr}</p>
+            <p>{time}</p>
         </div>
     )
 }
