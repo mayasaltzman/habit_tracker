1640d7c6d83d57aa70b0d7e69221db3d71195f65
Maya Saltzman <mayasaltzman3@gmail.com>
2023-10-27 11:36:57 -0400

    added calendar but it looks ugly

--- habit-tracker-react/src/components/pages/Home.js
+++ habit-tracker-react/src/components/pages/Home.js
@@ -1,8 +1,9 @@
-// import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from 'moment'
+import Calendar from 'react-calendar'
 
 const Home = () => {
-    // const [date, setDate] = useState(new Date()) //used to set current date and time
+    const [date, setDate] = useState(new Date()) //used to set date for calender
 
     // useEffect(() => {
     //     const timer = setInterval(() => { setDate(new Date()) }, 60000) //date is at an interval of 60 000 ms so clock updates each minute
@@ -17,7 +18,7 @@ const Home = () => {
     // getting date and time values
     // const hours = date.getHours()
     // const minutes = date.getMinutes()
-    
+
     //moment library just does this for me lol
     const curr = moment().format('MMMM Do YYYY')
     const time = moment().format('h:mm a')
@@ -31,6 +32,10 @@ const Home = () => {
             <h1>Welcome</h1>
             <p>{curr}</p>
             <p>{time}</p>
+
+            <div className='calendar-container'>
+                <Calendar onChange={setDate} value={date} />
+            </div>
         </div>
     )
 }
