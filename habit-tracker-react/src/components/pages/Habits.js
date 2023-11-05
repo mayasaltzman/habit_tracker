9236347429c9842d3b7b01d4b2555145add3c334
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-05 13:10:44 -0500

    added habit form contents

--- habit-tracker-react/src/components/pages/Habits.js
+++ habit-tracker-react/src/components/pages/Habits.js
@@ -3,7 +3,37 @@ import React from 'react'
 const Habits = () => {
   return (
     <div>
-      
+      <h1>Habits</h1>
+      <div id='habits-list'>
+        <ul>
+          <li>test</li>
+        </ul>
+      </div>
+      <h2>Add Habit</h2>
+      <form>
+        <label>
+          Habit:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Frequency:
+          <select>
+            <option value="daily">Daily</option>
+            <option value="weekly">Weekly</option>
+            <option selected value="bi-weekly">Bi-weekly</option>
+            <option value="monthly">Monthly</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <label>
+          Category:
+          <select>
+            <option value="dummy">Dummy</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   )
 }
--- habit-tracker-react/src/components/pages/Home.js
+++ habit-tracker-react/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import moment from 'moment'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
@@ -7,30 +7,12 @@ import './Home.css';
 const Home = () => {
     const [date, setDate] = useState(new Date()) //used to set date for calender
 
-    // useEffect(() => {
-    //     const timer = setInterval(() => { setDate(new Date()) }, 60000) //date is at an interval of 60 000 ms so clock updates each minute
-
-    //     // return () => clearInterval(timer)
-    //     return function cleanup() { //helps avoid memeory leaks
-    //         clearInterval(timer)
-    //     }
-
-    // })
-
-    // getting date and time values
-    // const hours = date.getHours()
-    // const minutes = date.getMinutes()
-
     //moment library just does this for me lol
     const curr = moment().format('MMMM D, YYYY')
     const time = moment().format('h:mm a')
 
     return (
         <div className='landing'>
-            {/* extracts time and returns as string */}
-            {/* <p>{String(hours - 12).padStart(2, '0')}:{String(minutes).padStart(2, '0')}</p> */}
-            {/* extracts datw and returns as string */}
-            {/* <p>{date.toLocaleDateString()}</p> */}
             <h1>{curr}</h1>
             <h2>{time}</h2>
 
