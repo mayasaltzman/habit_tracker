a4536c228e11c5bd71f4951145a69f50701ed012
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-05 13:29:09 -0500

    basic form stylings so easy to work with

--- habit-tracker-react/src/components/pages/Habits.js
+++ habit-tracker-react/src/components/pages/Habits.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Habits.css';
 
 const Habits = () => {
   return (
@@ -9,32 +10,37 @@ const Habits = () => {
           <li>test</li>
         </ul>
       </div>
-      <h2>Add Habit</h2>
-      <form>
-        <label>
-          Habit:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Frequency:
-          <select>
-            <option value="daily">Daily</option>
-            <option value="weekly">Weekly</option>
-            <option selected value="bi-weekly">Bi-weekly</option>
-            <option value="monthly">Monthly</option>
-            <option value="other">Other</option>
-          </select>
-        </label>
-        <label>
-          Category:
-          <select>
-            <option value="dummy">Dummy</option>
-            <option value="other">Other</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div id='add-habit'>
+        <h2>Add Habit</h2>
+        <div id='form-wrapper'>
+          <form id='habit-form'>
+            <label className='test'>
+              Habit:
+              <input type="text" name="name" />
+            </label>
+            <label className='form-content'>
+              Frequency:
+              <select>
+                <option selected value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="bi-weekly">Bi-weekly</option>
+                <option value="monthly">Monthly</option>
+                <option value="other">Other</option>
+              </select>
+            </label>
+            <label className='form-content'>
+              Category:
+              <select>
+                <option value="dummy">Dummy</option>
+                <option value="other">Other</option>
+              </select>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
     </div>
+
   )
 }
 
