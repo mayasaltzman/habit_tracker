b76c20f9ae448e763b41463e745edca8d033b948
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-09 10:46:56 -0500

    made inputs required

--- habit-tracker-react/src/components/pages/Habits.js
+++ habit-tracker-react/src/components/pages/Habits.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState}  from 'react'
 import './Habits.css';
 
 const Habits = () => {
+
+  // prevents page from refreshing
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Habits</h1>
+      {/* list of habits */}
       <div id='habits-list'>
         <ul>
           <li>test</li>
@@ -12,25 +19,25 @@ const Habits = () => {
       </div>
       <div id='add-habit'>
         <h2>Add Habit</h2>
+        {/* section of form input to add habits starts here*/}
         <div id='form-wrapper'>
-          <form id='habit-form'>
+          <form id='habit-form' onSubmit={handleSubmit}>
             <label className='test'>
               Habit:
-              <input type="text" name="name" />
+              <input type="text" name="name" required/>
             </label>
             <label className='form-content'>
               Frequency:
-              <select>
+              <select required>
                 <option selected value="daily">Daily</option>
                 <option value="weekly">Weekly</option>
                 <option value="bi-weekly">Bi-weekly</option>
                 <option value="monthly">Monthly</option>
-                <option value="other">Other</option>
               </select>
             </label>
             <label className='form-content'>
               Category:
-              <select>
+              <select required>
                 <option value="dummy">Dummy</option>
                 <option value="other">Other</option>
               </select>
@@ -38,6 +45,7 @@ const Habits = () => {
             <input type="submit" value="Submit" />
           </form>
         </div>
+        {/* form input ends here */}
       </div>
     </div>
 
